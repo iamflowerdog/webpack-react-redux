765d9d33f31da79b4c739ffecd784c675defb6c9
hjzgg <2570230521@qq.com>
2017-01-15 22:45:14 +0800

    hjz

--- src/main.js
+++ src/main.js
@@ -6,16 +6,18 @@ import routes from 'routes';
 import { createHashHistory, useBasename } from 'history';
 import { syncReduxAndRouter } from 'redux-simple-router';
 import { routeReducer } from 'redux-simple-router';
-var configureStore = require('./stores/configureStore');
 
-// Run our app under the /base URL.
+//初始化store
+var configureStore = require('./stores/configureStore');
+//配置路由Base路径 createHistory
 const history = useBasename(createHashHistory)({
   basename: '/',
 });
+//创建store
 const store = configureStore(window.__INITIAL_STATE__);
-
+//同步路由与Redux的状态
 syncReduxAndRouter(history, store);
-
+//渲染入口
 ReactDOM.render
 (
   <Provider store={store}>
